03c4e23a520e24da6b38d18c7e6ccd06d433d704
TornikeKalmaxelidze <tokokalmaxelidzee@gmail.com>
2024-12-28 16:35:09 +0400

    20 30 40 pages
    ragghac

--- catalog.js
+++ catalog.js
@@ -1,16 +1,18 @@
 const productsContainer = document.querySelector('#products');
 const prevButton = document.querySelector('#prev-btn');
 const nextButton = document.querySelector('#next-btn');
+const btn20 = document.getElementById('btn-20');
+const btn30 = document.getElementById('btn-30');
+const btn40 = document.getElementById('btn-40');
 
-let currentPage = 1; 
-const productsPerPage = 10;  
+let currentPage = 1;
+let productsPerPage = 10; 
 
 function fetchProducts(page) {
     fetch(`https://dummyjson.com/products?limit=${productsPerPage}&skip=${(page - 1) * productsPerPage}`)
         .then(response => response.json())
         .then(data => {
-            productsContainer.innerHTML = '';
-
+            productsContainer.innerHTML = ''; 
             data.products.forEach(product => {
                 const div = document.createElement('div');
                 div.classList.add('product');
@@ -18,7 +20,7 @@ function fetchProducts(page) {
                     <div class="card">
                         <img src="${product.thumbnail}" alt="${product.title}" style="width:100%">
                         <h1>${product.title}</h1>
-                        <p class="price">${product.price}</p>
+                        <p class="price">${product.price}$</p>
                         <p>${product.description.slice(0, 30)}...</p>
                         <p><button>Add to Cart</button></p>
                     </div>
@@ -27,20 +29,36 @@ function fetchProducts(page) {
             });
 
             prevButton.disabled = page === 1;  
-            nextButton.disabled = data.products.length < productsPerPage;  
+            nextButton.disabled = data.products.length < productsPerPage; 
         })
-        .catch(error => {
-            console.error('Error fetching products:', error);
-        });
+      
 }
 
 nextButton.addEventListener('click', () => {
-    currentPage++;  
+    currentPage++;
     fetchProducts(currentPage);
 });
 
 prevButton.addEventListener('click', () => {
-    currentPage--;  
+    currentPage--;
+    fetchProducts(currentPage);
+});
+
+btn20.addEventListener('click', () => {
+    productsPerPage = 20;
+    currentPage = 1; 
+    fetchProducts(currentPage);
+});
+
+btn30.addEventListener('click', () => {
+    productsPerPage = 30;
+    currentPage = 1; 
+    fetchProducts(currentPage);
+});
+
+btn40.addEventListener('click', () => {
+    productsPerPage = 40;
+    currentPage = 1; 
     fetchProducts(currentPage);
 });
 
